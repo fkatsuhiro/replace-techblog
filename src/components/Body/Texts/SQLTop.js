51492dfa1b7b47fe2301fd3ef354370b8dfbdab1
fkatsuhiro <nakayoshiryutokatsu@gmail.com>
2024-12-10 09:05:50 +0900

    repaire the json data

--- src/components/Body/Texts/SQLTop.js
+++ src/components/Body/Texts/SQLTop.js
@@ -4,7 +4,7 @@ import { Link, useLocation } from 'react-router-dom';
 import '../../../App.css';
 
 function SQLTop() {
-    const location = useLocation();
+    const [SQLTexts, setSQLTexts] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
